645faf2a24b974111e35d2110e63cb7f68646ea1
Aluno <me@me.com>
2014-07-15 09:59:02 -0300

    Task 1.03 for oo

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -6,23 +6,32 @@ import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
-	
-	private int totalcent;
+
+	private int totalCent;
+	private int totalGeral;
 	private ComponentsFactory factory;
-	
+
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
 	public void insertCoin(Coin coin) {
-		totalcent = coin.getValue();
-		int Decimal =  totalcent/100;
-		int Centavos = totalcent%100;
-		this.factory.getDisplay().info("Total: US$ " + Decimal + "." + Centavos);
-		
-		
+		totalCent = coin.getValue();
+		int Decimal = totalCent / 100;
+		int Centavos = totalCent % 100;
+		this.factory.getDisplay()
+				.info("Total: US$ " + Decimal + "." + Centavos);
 	}
 
+	public void insertCoins(Coin coins) {
+		totalCent = coins.getValue();
+		totalGeral = totalGeral + totalCent;
+		int Decimals = totalGeral / 100;
+		int Centavo = totalGeral % 100;
+		this.factory.getDisplay()
+				.info("Total: US$ " + Decimals + "." + Centavo);
+
+	}
 
 }
