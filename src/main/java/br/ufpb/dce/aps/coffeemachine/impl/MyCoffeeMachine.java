b39d3f37c299e66407c68c31d3f6bdef3180b23d
Aluno <me@me.com>
2014-07-18 10:10:26 -0300

    Task 1.04 finished for oo

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,6 +1,7 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
@@ -15,8 +16,16 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		this.factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin coin) {
-		this.totalCents += coin.getValue();
-		this.factory.getDisplay().info ("Total: US$ "+this.totalCents/100+"." + this.totalCents%100);
+	public void insertCoin(Coin coin)throws CoffeeMachineException {
+		try{
+			this.totalCents += coin.getValue();
+			this.factory.getDisplay()
+			.info("Total: US$ "+this.totalCents/100+"." + this.totalCents%100);
+		}
+		catch(NullPointerException e){
+			throw new CoffeeMachineException("Esta moeda não é aceita"); 
+		
+		}
 	}
+	
 }
