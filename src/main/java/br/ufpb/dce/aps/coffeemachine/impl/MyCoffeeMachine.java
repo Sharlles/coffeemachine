76618da7ac39706482d4d1828e2f33c191d52556
Aluno <me@me.com>
2014-07-21 08:41:05 -0300

    Task 1.06 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -4,6 +4,7 @@ import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
@@ -31,7 +32,10 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	public void cancel() {
 		if(totalCents == 0) {
 			throw new CoffeeMachineException("Sem moedas");
-			
+		}else{
+			this.factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+			this.factory.getCashBox().release(Coin.halfDollar);
+			this.factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 		}
 		
 	}
