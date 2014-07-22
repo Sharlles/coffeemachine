4a21157b445e71e15221b2420ddc84d6efbc006c
Aluno <me@me.com>
2014-07-22 09:20:32 -0300

    Task 1.09 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,11 +1,14 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import static org.mockito.Matchers.anyDouble;
+
 import java.util.ArrayList;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Drink;
 import br.ufpb.dce.aps.coffeemachine.Messages;
 import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
 
@@ -14,6 +17,7 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	private int totalCents;
 	private ComponentsFactory factory;
 	ArrayList< Coin > moedas = new ArrayList< Coin >();
+	private Drink drink;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
@@ -47,13 +51,31 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		if(totalCents == 0) {
 			throw new CoffeeMachineException("Sem moedas");
 		}else{
-			this.factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+			this.factory.getDisplay().warn(Messages.CANCEL);
 			for(int i = 0; i< moedas.size() ; i++){
 				this.factory.getCashBox().release(moedas.get(i));
 			}
-			this.factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
+			this.factory.getDisplay().info(Messages.INSERT_COINS);
 		}
 		
+	}
+
+	public void select(Drink drink) {
+	
+		this.drink = drink;
+		this.factory.getCupDispenser().contains(1);
+		this.factory.getWaterDispenser().contains(anyDouble());
+		this.factory.getCoffeePowderDispenser().contains(anyDouble());
+		
+		this.factory.getDisplay().info(Messages.MIXING);
+		this.factory.getCoffeePowderDispenser().release(anyDouble());
+		this.factory.getWaterDispenser().release(anyDouble());
+		this.factory.getDisplay().info(Messages.RELEASING);
+		
+		this.factory.getCupDispenser().release(1);
+		this.factory.getDrinkDispenser().release(anyDouble());
+		this.factory.getDisplay().info(Messages.TAKE_DRINK);
+		this.factory.getDisplay().info(Messages.INSERT_COINS);	
 	}
 	
 }
